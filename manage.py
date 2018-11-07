03050301466bc54b14db87758eb10b919f681d4f
LiuMeng <272049690@qq.com>
2018-11-07 16:50:12 +0800

    使用pycharm操作git，控制代码版本

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+# flask 实例对象的初始化
 app = Flask(__name__)
 
 
@@ -10,11 +11,12 @@ class Config(object):
 app.config.from_object(Config)
 
 
+# 定义视图函数
 @app.route("/index")
 def index():
     return "ind"
 
 
+# 运行项目
 if __name__ == '__main__':
     app.run()
-
