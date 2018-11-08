c7ea183a5eb460769aa5faf8ff4482fdeebbc4f7
LiuMeng <272049690@qq.com>
2018-11-08 21:22:45 +0800

    实现首页根路由 以及 网站logo小图片的显示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,3 +1,6 @@
+from flask import current_app
+from flask import render_template
+
 from . import index_blu
 
 # 定义视图函数
@@ -8,5 +11,9 @@ def index():
     # logging.warning("警告错误！")
     # logging.info("普通日志信息！")
     # logging.debug("调试信息")
+    title = "首页-新经资讯网"
+    return render_template("news/index.html",title=title)
 
-    return "hello news!"
+@index_blu.route("/favicon.ico")
+def favicon():
+    return current_app.send_static_file("news/favicon.ico")
