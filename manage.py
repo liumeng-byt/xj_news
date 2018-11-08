97fa4b01e983f2111d486ca6c3c27f48e47bae06
LiuMeng <272049690@qq.com>
2018-11-08 15:06:47 +0800

    分离manage.py 中的代码-分离配置信息代码

--- manage.py
+++ manage.py
@@ -3,32 +3,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 from flask_session import Session
-import base64, os
 from  flask_migrate import Migrate, MigrateCommand
-
 from redis import StrictRedis
+from config import Config
 
-
-class Config(object):
-    # debug模式
-    DEBUG = True
-    # 数据库的配置信息
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # redis配置
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = 6379
-    REDIS_SESSION_DB = 1
-    REDIS_DATA_DB = 0
-
-    SECRECT_KEY = base64.b64encode(os.urandom(48))  # 设置密钥
-
-    # flask_session 的配置信息
-    SESSION_TYPE = "redis"  # 指定 session 保存到 redis 中
-    SESSION_USE_SIGNER = True  # 让 cookie 中的 session_id 被加密签名处理
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_SESSION_DB)  # 使用 redis 的实例
-    PERMANENT_SESSION_LIFETIME = 86400  # session 的有效期，单位是秒
 
 
 # flask 实例对象的初始化
