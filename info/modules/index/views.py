b2f254e45721645e52aa85f1335ba3d5f7c18f1e
LiuMeng <272049690@qq.com>
2018-11-11 23:29:01 +0800

    实现点击排行榜（无序号颜色）

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -2,7 +2,8 @@ from flask import current_app
 from flask import render_template
 from flask import session
 
-from info.models import User
+from info import constants
+from info.models import User, News
 from . import index_blu
 
 # 定义首页视图函数
@@ -16,10 +17,17 @@ def index():
             user = User.query.get(user_id)
         except Exception as e:
             current_app.logger.error(e)
+    news_list=None
+    try:
+        news_list = News.query.order_by(News.clicks.desc()).limit(constants.HOME_PAGE_MAX_NEWS).all()
+        # print(news_list)
+    except Exception as e:
+        current_app.logger.error(e)
 
 
     title = "首页-新经资讯网"
-    return render_template("news/index.html",title=title,user=user)
+    return render_template("news/index.html",title=title,user=user,news_list=news_list)
+
 
 
 # 显示logo图标的视图函数
