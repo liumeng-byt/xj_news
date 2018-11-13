9271086423aaac4b18ccb2a1e2ff6ddd1a703e61
LiuMeng <272049690@qq.com>
2018-11-13 23:02:32 +0800

    实现用户收藏功能-后端逻辑代码

--- info/modules/new/views.py
+++ info/modules/new/views.py
@@ -1,13 +1,15 @@
-from flask import abort
+from flask import abort, jsonify
 from flask import current_app
 from flask import g
 from flask import render_template
+from flask import request
 from flask import session
 
 from info import constants
 from info import db
 from info.models import News, User
 from info.utils.common import user_login_data
+from info.utils.response_code import RET
 from . import news_blu
 
 
@@ -49,3 +51,47 @@ def detail(news_id):
                            news_list=news_list,
                            news=news
                            )
+
+
+@news_blu.route("/news_collect",methods=["POST"])
+@user_login_data
+def news_collect():
+    """新闻收藏"""
+    user = g.user
+    json_data = request.json
+    news_id = json_data.get("news_id")
+    action = json_data.get("action")
+
+    #判断是否已登录
+    if not user:
+        return jsonify(errno=RET.SESSIONERR,errmsg="用户未登陆")
+
+    if not news_id:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    if action not in ("collect","cancel_collect"):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询数据失败")
+
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg="新闻数据不存在")
+
+    if action == "collect":
+        user.collection_news.append(news)
+    else:
+        user.collection_news.remove(news)
+
+    try:
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=RET.DBERR, errmsg="保存失败")
+
+    #返回结果
+    return jsonify(errno=RET.OK, errmsg="操作成功")
