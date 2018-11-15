145f52c00f8eb92cc150918bd3e0a4502985547d
LiuMeng <272049690@qq.com>
2018-11-15 20:52:16 +0800

    显示个人中心的基本信息

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,5 +1,6 @@
 from flask import g
 from flask import render_template
+from flask import request
 
 from info import user_login_data
 from info.modules.profile import profile_blu
@@ -14,8 +15,21 @@ def user_info():
     user = g.user
 
 
-
-
     return render_template("news/user.html",
                             user=user
                            )
+
+@profile_blu.route("/user_base_info",methods=["POST","GET"])
+@user_login_data
+def user_base_info():
+    """修改个人基本信息"""
+    # 获取登陆信息
+    if request.method == "GET":
+        user = g.user
+
+        return render_template("news/user_base_info.html",
+                           user=user
+                           )
+    else:
+        """POST"""
+        pass
